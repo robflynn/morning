ed3a32a3ef18c227399c196714d564e825649d37
Rob Flynn <rob@thingerly.com>
2018-12-29 07:39:38 -0500

    Add basic markup for chat view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,5 @@ class PagesController < ApplicationController
 #  before_action :authenticate_user!
 
   def root
-    @projects = current_user.projects
   end
 end
--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,81 +1,107 @@
 <template>
-  <div class="morning--app">
-    <SplitPanelView>
-      <aside class="sidebar">
-        <ProjectsList :projects="projects" />      
-      </aside>        
+  <div class="morning doze-app">
+    <div class="doze-split-view">
+      <aside class="doze-app--sidebar morning--sidebar">
+        <div class="doze-accordion">
+          <section>
+            <h2>Projects</h2>
+            <div>
+              <ul>
+                <li>Project 1</li>
+                <li>Project 2</li>
+                <li>Project 3</li>
+                <li>Project 4</li>
+              </ul>
+            </div>
+          </section>
 
-      <main class="main">
-        <router-view></router-view>
-      </main>        
-    </SplitPanelView>
+          <section>
+            <h2>Messages</h2>
+            <div>
+              <ul>
+                <li>Trent</li>
+                <li>Elton</li>
+                <li>David</li>
+              </ul>
+            </div>
+          </section>
+
+          <section>
+            <h2>Users</h2>
+            <div>
+              <ul>
+                <li>User A</li>
+                <li>User B</li>
+                <li>User C</li>
+                <li>User D</li>
+                <li>User E</li>
+              </ul>
+            </div>
+          </section>
+        </div>
+      </aside>
+
+      <main class="doze-app--main morning--main">
+
+        <div class="doze-split-view">
+
+          <div class="chat-messages">
+            <div class="message">
+              <div class="avatar"></div>
+              <span class="name">Person Name</span>
+              <span class="time">@ 23:00</span>
+              <div class="body">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas a urna non dolor gravida sagittis. Vestibulum eget porta ligula. Nulla lobortis a lacus nec fringilla. Cras ornare lacus ex, quis efficitur magna molestie eu. In hac habitasse platea dictumst. Donec egestas nibh a dolor ornare ornare. Nulla ut mi fermentum, dapibus nisi in, aliquam lectus.</p>
+
+                <p>Phasellus volutpat tellus eu bibendum pharetra. Praesent <a href="http://thingerly.com/">euismod</a>, sapien nec convallis bibendum, nibh ex malesuada felis, sit amet interdum ex nisl ut libero. Vestibulum placerat tellus eget pretium pretium. Proin fermentum ultricies fermentum. Morbi at nunc fringilla, ullamcorper nulla quis, aliquam leo. Praesent sit amet porttitor dolor. Proin condimentum finibus risus ac posuere. Morbi faucibus laoreet porta. Etiam posuere hendrerit tristique.</p>
+
+                <span class="attachment attachment--image"><img src="http://placekitten.com/450/320" /></span>
+              </div>
+            </div>
+          </div>
+
+          <aside class="doze-app--sidebar project--sidebar">
+            Another Sidebar
+          </aside>
+
+        </div>
+      </main>
+    </div>
   </div>
 </template>
 
 <script>
-  import ProjectsList from 'packs/components/projects_list'
-  import ProjectView from 'packs/components/project_view'
-  import SplitPanelView from 'packs/components/split_panel_view'  
-
   export default {
-    routes: [],
-
-    components: {
-      'ProjectsList': ProjectsList,
-      'ProjectView': ProjectView,
-      'SplitPanelView': SplitPanelView,
-    },
-
-    data() {
-      let projects = [
-        {
-          id: 1,
-          name: "project 1"
-        },
-        {
-          id: 2,
-          name: "project 2"
-        },
-        {
-          id: 3,
-          name: "project 3"
-        }
-      ]
-
-      return {
-        projects: projects,
-        currentProject: null
-      }
-    },
-
-    mounted() {
-      if (this.projects.length > 0) {
-        this.currentProject = this.projects[0]
-      }      
-    }
   }
 </script>
 
-<style lange="scss">
+<style lange="scss">  
   .col {
     display: flex;
     flex-direction: column;
   }
+
+  .row {
+    display: flex;
+    flex-direction: row;
+  }
 </style>
 
 <style lang="scss" scoped>
-  .morning--app {
+  .doze-app {
     background: green;
     display: flex;
     flex: 1;
   }
 
-  .sidebar {
-    width: 250px;
+  .doze-app--sidebar {
+    background: blue;
+    min-width: 200px;
   }
 
-  .main {
-    width: 100%;
+  .doze-split-view {
+    display: flex;
+    flex-direction: row;
     height: 100%;
   }
 </style>
