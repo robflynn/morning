87eda55501ab2e6efd01338f8f2d08434a99c882
Rob Flynn <rob@thingerly.com>
2018-12-31 01:40:11 -0500

    mount action cable

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
 
+
   namespace :api, constraints: { format: 'json' } do
     resources :chat_messages
   end
 
   root to: 'pages#root'
+
+  mount ActionCable.server, at: '/cable'  
 end
