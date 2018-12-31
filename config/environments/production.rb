030e4d5ef4f359e44439fb54a6a9172e6cc068ed
Rob Flynn <rob@thingerly.com>
2018-12-31 01:49:13 -0500

    Set heroku cable url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,7 +45,6 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
@@ -93,4 +92,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_cable.url = 'wss://ampm.herokuapp.com/cable'  
 end
