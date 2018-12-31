879554fc25a884ce1635f10eca8a93253bfe444d
Rob Flynn <rob@thingerly.com>
2018-12-31 03:46:49 -0500

    create pill view

---
+++ app/javascript/packs/components/pill_cell.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="pill-cell">
+        <span><slot></slot></span>
+
+        <span v-if="this.count > 0" class="pill">{{ this.count }}</span>
+    </div>
+</template>
+
+<script>
+    export default {        
+        props: {
+            count: {
+                default: 0,
+                type: Number
+            }
+        },
+
+        updated() {
+            console.log("it changededed")
+        }
+    }
+</script>
+
+<style lang="scss">
+
+    .pill-cell {
+        display: flex;
+        flex-direction: row;        
+        flex: 1;
+        padding: 5px;
+
+        span:first-child {
+            padding-top: 5px;
+            width: 100%;
+        }
+
+        .pill {
+            $size: 5px;
+
+            font-size: 0.75em;
+            background: rgba(255, 0, 0, 0.8);
+            padding: $size;
+            border-radius: $size;
+            min-width: 5 * $size;
+            align-self: flex-end;
+            text-align: center;
+            font-weight: bold
+        }
+    }
+</style>
--- app/javascript/packs/components/projects_list.vue
+++ app/javascript/packs/components/projects_list.vue
@@ -1,87 +1,60 @@
 <template>
     <ul class="projects-list">
         <li v-for="(project, index) in projects" :key="index" class="project">
-            <div class="pill-cell">
-                <span>{{ project.name }}</span>
-
-                <span v-if="project.counter > 0" class="pill">{{ project.counter }}</span>
-            </div>
+            <pill-cell :count="project.counter">{{ project.name }}</pill-cell>
         </li>
     </ul>
 </template>
 
 <script>
-     export default {
-        data() {
-            return {
-                projects: []
-            }
-        },
+ import PillCell from './pill_cell.vue'
 
-        methods: {
-            getProjects() {
-                Morning.getProjects()                                        
-                   .then((projects) => {
-                    console.log("A")
-                    console.log(projects)
-                                        console.log("2")
-                        this.projects = projects
-                    })
-            }
-        },
 
-        mounted() {
-            this.getProjects()
-        },
-
-        updated() {           
-            console.log("Q")
-            console.log("testing")
-            console.log(this)
-            console.log(this.projects)
+ export default {
+    data() {
+        return {
+            projects: []
         }
-    }
+    },
+
+    components: {
+        "pill-cell": PillCell
+    },
+
+    methods: {
+        getProjects() {
+            Morning.getProjects()                                        
+               .then((projects) => {
+                console.log("A")
+                console.log(projects)
+                                    console.log("2")
+                    this.projects = projects
+                })
+        }
+    },
+
+    mounted() {
+        this.getProjects()
+    },
+}
 </script>   
 
 <style lang="scss" scoped>
-    .projects-list {
-        display: flex;
-        flex-direction: column;
+.projects-list {
+    display: flex;
+    flex-direction: column;
 
-        .project {
-            padding: 5px;
-            cursor: pointer;
+    .project {
+        padding: 5px;
+        cursor: pointer;
 
-            &:hover {
-                background: rgba(255, 255, 255, 0.1);
-            }
-
-            a {
-                color: #fff;
-            }
-        }
-    }    
-
-    .pill-cell {
-        display: flex;
-        flex-direction: row;        
-        flex: 1;
-
-        span:first-child {
-            width: 100%;
+        &:hover {
+            background: rgba(255, 255, 255, 0.1);
         }
 
-        .pill {
-            $size: 5px;
-
-            font-size: 0.75em;
-            background: rgba(255, 0, 0, 0.8);
-            padding: $size;
-            border-radius: $size;
-            min-width: 5 * $size;
-            align-self: flex-end;
-            text-align: center;
-            font-weight: bold
+        a {
+            color: #fff;
         }
     }
+}    
 </style>
